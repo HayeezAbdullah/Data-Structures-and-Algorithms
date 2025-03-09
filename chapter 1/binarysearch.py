a6e2649e091e5fa2e33ad5012a255853b6c7b6ec
Hayeez Abdullah <hayeezabdullah2k5@gmail.com>
2025-03-09 20:18:47 +0530

    Binary Search Algorithm

--- chapter 1/binarysearch.py
+++ chapter 1/binarysearch.py
@@ -1,1 +1,17 @@
-print("heloo world")
+# Binary Search
+my_list = [1,3,5,7,9]
+
+def binary_search(list,item):
+    low = 0
+    high = len(list-1)
+
+    while low <= high:
+        mid = (low+high)
+        guess = list[mid]
+        if guess > item:
+            high = mid-1
+        else:
+            low = mid+1
+        return None
+    
+binary_search(my_list,3)
