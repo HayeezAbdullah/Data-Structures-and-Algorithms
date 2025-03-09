12d40867959660596a52ddbc3f5d9ee03f6ec10c
Hayeez Abdullah <hayeezabdullah2k5@gmail.com>
2025-03-09 20:35:13 +0530

    Fix Bug & Test Algorithm

--- chapter 1/binarysearch.py
+++ chapter 1/binarysearch.py
@@ -1,17 +1,22 @@
 # Binary Search
-my_list = [1,3,5,7,9]
 
-def binary_search(list,item):
+def binary_search(lst, item):
     low = 0
-    high = len(list-1)
+    high = len(lst) - 1
 
     while low <= high:
-        mid = (low+high)
-        guess = list[mid]
-        if guess > item:
-            high = mid-1
+        mid = (low + high) // 2
+        guess = lst[mid]  
+
+        if guess == item:
+            return mid
+        elif guess > item:
+            high = mid - 1
         else:
-            low = mid+1
-        return None
+            low = mid + 1
     
-binary_search(my_list,3)
+    return None  
+
+# Testing
+my_list = [1, 3, 5, 7, 9]
+print(binary_search(my_list, 3)) 
